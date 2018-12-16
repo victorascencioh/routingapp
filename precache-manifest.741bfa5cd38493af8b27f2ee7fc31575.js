3fe2b1664d0d293885b06df889d7b5ca60524bb4
victorascencioh <victorascencioh@gmail.com>
2018-12-16 20:01:47 +0100

    Updates

---
+++ precache-manifest.741bfa5cd38493af8b27f2ee7fc31575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c4612352058cebcdaf",
+    "url": "/routingapp/static/js/runtime~main.61c46123.js"
+  },
+  {
+    "revision": "a39d3eb675f4fdd1ab16",
+    "url": "/routingapp/static/js/main.a39d3eb6.chunk.js"
+  },
+  {
+    "revision": "b8c909e68adc5b9c4138",
+    "url": "/routingapp/static/js/1.b8c909e6.chunk.js"
+  },
+  {
+    "revision": "a39d3eb675f4fdd1ab16",
+    "url": "/routingapp/static/css/main.fea42c81.chunk.css"
+  },
+  {
+    "revision": "80438c7d5afea6b2d549c316107cf80f",
+    "url": "/routingapp/index.html"
+  }
+];
